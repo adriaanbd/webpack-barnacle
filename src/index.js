926af4398b1934aa9fca6f0e744f0733d531e740
Adriaan Beiertz <adriaanbd@gmail.com>
2019-08-30 11:26:38 -0500

    style and css loader included in webpack config

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import _ from 'lodash';
+import './style.css'; // we can import the css file because of style-loader and css-loader in webpack config file
 
 function component() {
   const element = document.createElement('div');
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,19 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  // in order to import a CSS file from within a JS module, we need to install and add style-loader and css-loader to module configuration. So, after running `npm install --save-dev style-loader css-loader` in terminal, add the following:
+  module: {
+    rules: [
+      {
+        test: /\.css/, // webpack uses regex to determine which files it should look for and save to a specific loader. In this case, any file ending in .css will eb server to style-loader and css-loader.
+        use: [ // when the modules is run, a <style> tag with stringified css will be inserted into the <head> of the html file.
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
   }
 };
