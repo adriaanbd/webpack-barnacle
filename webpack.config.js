f2238ecf4e30f4745e3b7af6052c98413002b3e8
Adriaan Beiertz <adriaanbd@gmail.com>
2019-08-30 13:41:55 -0500

    cleaning up for other output management

--- webpack.config.js
+++ webpack.config.js
@@ -9,40 +9,4 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
-  // in order to import a CSS file from within a JS module, we need to install and add style-loader and css-loader to module configuration. So, after running `npm install --save-dev style-loader css-loader` in terminal, add the following:
-  module: {
-    rules: [
-      {
-        test: /\.css/, // webpack uses regex to determine which files it should look for and save to a specific loader. In this case, any file ending in .css will eb server to style-loader and css-loader.
-        use: [ // when the modules is run, a <style> tag with stringified css will be inserted into the <head> of the html file.
-          'style-loader', // npm install --save-dev style-loader css-loader
-          'css-loader'
-        ]
-      },
-      { // Using the file-loader we can easily incorporate images and icons
-        test: /\.(png|svg|jpg|gif)$/,
-        use: [ // The html-loader handles <img src="./my-image.png" />
-          'file-loader' // npm install --save-dev file-loader
-        ]
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/, // fonts use file-loader too
-        use: [
-          'file-loader'
-        ]
-      },
-      {
-        test: /\.(csv|tsv)$/,
-        use: [
-          'csv-loader'
-        ]
-      },
-      {
-        test: /\.xml$/,
-        use: [
-          'xml-loader'
-        ]
-      }
-    ]
-  }
 };
