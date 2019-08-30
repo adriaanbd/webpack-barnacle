1fa5582eaf8306f5531508d0ab7c718d632e8db5
Adriaan Beiertz <adriaanbd@gmail.com>
2019-08-30 11:57:45 -0500

    added file-loader to npm

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import _ from 'lodash';
 import './style.css'; // we can import the css file because of style-loader and css-loader in webpack config file
+import Pic from './image.jpg'; // we can import the image because of file-loader in webpack config file
 
 function component() {
   const element = document.createElement('div');
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
+
+  const myImage = new Image();
+  myImage.src = Pic;
+  myImage.className = 'pic'
+
+  element.appendChild(myImage);
 
   return element;
 }
